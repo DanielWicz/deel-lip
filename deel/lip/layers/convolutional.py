a201c209eb3b75574e889753e0a7a2651ef755c3
Daniel Wiczew <37775482+DanielWicz@users.noreply.github.com>
2025-10-11 15:59:01 +0200

    Fix SpectralConv2D weight serialization

--- deel/lip/layers/convolutional.py
+++ deel/lip/layers/convolutional.py
@@ -281,6 +281,21 @@ class SpectralConv2D(Conv2D, LipschitzLayer, Condensable):
         base_config = super(SpectralConv2D, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
+    def save_own_variables(self, store):
+        super().save_own_variables(store)
+        store["sn"] = self.u.numpy()
+        store["sigma"] = self.sig.numpy()
+        store["wbar"] = self.wbar.numpy()
+
+    def load_own_variables(self, store):
+        super().load_own_variables(store)
+        if "sn" in store:
+            self.u.assign(store["sn"])
+        if "sigma" in store:
+            self.sig.assign(store["sigma"])
+        if "wbar" in store:
+            self.wbar.assign(store["wbar"])
+
     def condense(self):
         wbar, u, sigma = reshaped_kernel_orthogonalization(
             self.kernel,
@@ -529,6 +544,21 @@ class SpectralConv2DTranspose(Conv2DTranspose, LipschitzLayer, Condensable):
         }
         base_config = super().get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+    def save_own_variables(self, store):
+        super().save_own_variables(store)
+        store["sn"] = self.u.numpy()
+        store["sigma"] = self.sig.numpy()
+        store["wbar"] = self.wbar.numpy()
+
+    def load_own_variables(self, store):
+        super().load_own_variables(store)
+        if "sn" in store:
+            self.u.assign(store["sn"])
+        if "sigma" in store:
+            self.sig.assign(store["sigma"])
+        if "wbar" in store:
+            self.wbar.assign(store["wbar"])
 
     def condense(self):
         wbar, u, sigma = reshaped_kernel_orthogonalization(
